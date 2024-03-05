f881458baa4bc7d7415ea8b2b6bd063fc666eea4
paul-enno <paul.enno@gmail.com>
2024-03-05 11:03:36 +0100

    Removed files from wrong loaction.

--- custom_components/const.py
+++
@@ -1,9 +0,0 @@
-"""Constants for the Amazon Bedrock Agent integration."""
-from typing import Final
-
-DOMAIN: Final = "bedrock_agent"
-
-CONST_KEY_ID: Final = "key_id"
-CONST_KEY_SECRET: Final = "key_secret"
-CONST_REGION: Final = "region"
-CONST_MODEL_ID: Final = "model_id"
